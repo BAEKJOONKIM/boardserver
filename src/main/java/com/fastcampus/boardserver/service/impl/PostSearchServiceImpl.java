2bd598b272ab382a3a70472b6d589403e380230d
BAEKJOON <majaua@naver.com>
2024-07-15 22:34:01 +0900

    post, tag make

--- src/main/java/com/fastcampus/boardserver/service/impl/PostSearchServiceImpl.java
+++ src/main/java/com/fastcampus/boardserver/service/impl/PostSearchServiceImpl.java
@@ -28,4 +28,15 @@ public class PostSearchServiceImpl implements PostSearchService {
         }
         return postDTOList;
     }
+
+    @Override
+    public List<PostDTO> getPostByTag(String tagName) {
+        List<PostDTO> postDTOList = null;
+        try {
+            postDTOList = postSearchMapper.getPostByTag(tagName);
+        }catch(RuntimeException e){
+            log.error("selectPosts 메서드 실패", e.getMessage());
+        }
+        return postDTOList;
+    }
 }
